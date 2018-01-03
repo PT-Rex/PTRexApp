7878925aa5e69ecf6d4c7ed715bcb8de59ec8d39
eacallam <eacallam@yahoo.com>
2018-01-03 18:37:47 -0500

    continued work

---
+++ PTRex/Models/ProfilePixBuddy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTRex.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+
+    [MetadataType(typeof(ProfilePixBuddy))]
+    public partial class ProfilePix
+    {
+
+    }
+
+    sealed class ProfilePixBuddy
+    {
+        public int ImageID { get; set; }
+        [Display(Name = "Upload File")]
+        public string ImagePath { get; set; }
+        public int UserProfileID { get; set; }
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
